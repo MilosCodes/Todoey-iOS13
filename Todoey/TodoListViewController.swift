834d00248f23dfbd3dcb0d1bb70de82afc6f8c4b
MilosCodes <49164774+MilosCodes@users.noreply.github.com>
2020-10-17 21:00:28 +0200

    new project
    new project

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+  var itemArray = ["Banana",
+                   "Apple",
+                   "Carrot",
+                   "Your Mom"
+  ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      tableView.register(UITableViewCell.self, forCellReuseIdentifier: "toDoCellId")
+    }
+  //MARK: -tableView Data Source methods
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return itemArray.count
+  }
+
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "toDoCellId", for: indexPath)
+    cell.textLabel?.text = itemArray[indexPath.row]
+    return cell
+  }
+
+  //MARK: -tableViewDelegate methods
+
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    print(itemArray[indexPath.row])
+
+    if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+      tableView.cellForRow(at: indexPath)?.accessoryType = .none
+    } else {
+      tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+    }
+    tableView.deselectRow(at: indexPath, animated: true)
+
+  }
+  @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    var textField = UITextField()
+    let alert = UIAlertController(title: "Add new todoey Item", message: "", preferredStyle: .alert)
+    let action = UIAlertAction(title: "Add item", style: .default) { (action) in
+      // what will happen once the user clicks the add item button on our UIAlert
+      self.itemArray.append(textField.text ?? "Unknow Item")
+
+      self.tableView.reloadData()
+    }
+    alert.addTextField { (alertTextField) in
+      alertTextField.placeholder = "Writte something"
+      textField = alertTextField
+    }
+    alert.addAction(action)
+    present(alert, animated: true, completion: nil)
+  }
+}
+
